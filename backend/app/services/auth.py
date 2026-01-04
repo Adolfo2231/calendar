5c38b30f87e5561d6c73b4fa7a7d1f626b10b76b
Adolfo <adolfo.rodriguez2231@gmail.com>
2026-01-03 22:38:55 -0400

    feat: add email and username uniqueness validation in register_user

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.repository import UserRepository
+from app.exception.bussines import EmailExistError, UsernameExistError
 
 class AuthService:
     def __init__(self, session: AsyncSession):
@@ -8,6 +9,12 @@ class AuthService:
 
     async def register_user(self, user_data: dict):
         """Registra un nuevo usuario"""
+        if await self.user_repository.get_by_email(user_data["email"]):
+            raise EmailExistError()
+
+        if await self.user_repository.get_by_username(user_data["username"]):
+            raise UsernameExistError("Username already registered")
+        
         user = await self.user_repository.create(user_data)
         await self.session.commit()  # Commit a nivel de servicio
         return user  # Devolvemos el objeto SQLAlchemy directamente ya que el Pydantic lo convierte automáticamente en el ResponseUser del endpoint
