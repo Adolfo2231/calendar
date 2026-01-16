a163aac2464bea1c847aefb0a3fb332b6b9a486e
Adolfo <adolfo.rodriguez2231@gmail.com>
2026-01-16 13:12:25 -0400

    feat: register event router in API v1

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .auth import router as auth_router
+from .event import router as event_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth")
+api_router.include_router(event_router, prefix="/event")
 
 __all__ = ["api_router"]
